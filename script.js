dbe2d9b5466a2ad14a9d57ad5905ff9454723f67
TAK1ST <90909909+TAK1ST@users.noreply.github.com>
2024-07-09 23:26:31 +0700

    create backtotoppage; footer; header (some bugs); responsive footer; update slideshow; create dropdown

---
+++ script.js
@@ -0,0 +1,100 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Automatic slideshow
+let autoSlideIndex = 0;
+autoShowSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("Slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function autoShowSlides() {
+    let i;
+    let slides = document.getElementsByClassName("Slides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    autoSlideIndex++;
+    if (autoSlideIndex > slides.length) {
+        autoSlideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[autoSlideIndex - 1].style.display = "block";
+    dots[autoSlideIndex - 1].className += " active";
+    setTimeout(autoShowSlides, 4000); // Change image every 4 seconds
+}
+
+// Scroll to reveal
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+// Hamburger menu
+const menuIcon = document.getElementById('menu-icon');
+const hiddenMenu = document.getElementById('hidden-menu');
+
+menuIcon.addEventListener('click', () => {
+    hiddenMenu.classList.toggle('taskbar__hidden-menu--visible');
+});
+
+// Back to top button
+var mybutton = document.getElementById("back-to-top-btn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
